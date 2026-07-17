b3e66f760b23c6bd4bbf8e11d36a6ee1391219c9
Nandhak kishore <nandhakkishore31@gmail.com>
2026-07-17 10:22:56 +0530

    Create TopKFreequent.java

---
+++ TopKFreequent.java
@@ -0,0 +1,17 @@
+public class TopKFreequent {
+    public static int[] extract(int[] arr, int k){
+        Map<Integer, Integer> map = new LinkedHashMap<>();
+        for(int each:arr){
+            int temp = map.
+                    getOrDefault(each, 0)+1;
+            map.put(each,temp);
+        }
+        List<Map.Entry<Integer,Integer>> tempList = 
+        new ArrayList<>(map.entrySet());
+        tempList.sort(Map.Entry.comparingByValue());
+        Collections.reverse(tempList);
+        int[] result=new int[k];
+        for(int index=0;index<k;index++)
+            result[index]=tempList.get(index).getKey();
+        return result;
+    }
